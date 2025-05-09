ed7b674fded66e5845b5832044f843a726b2e515
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-09 14:45:01 +0800

    add component

---
+++ app/src/main/java/com/abcd/e_kalontong/utils/hilt/AppModule.kt
@@ -0,0 +1,52 @@
+package com.abcd.e_kalontong.utils.hilt
+
+import com.abcd.aplikasipenjualanplafon.utils.TanggalDanWaktu
+import com.abcd.e_kalontong.data.database.api.ApiService
+import com.abcd.e_kalontong.utils.Constant
+import com.abcd.e_kalontong.utils.KataAcak
+import com.abcd.e_kalontong.utils.LoadingAlertDialog
+import com.abcd.e_kalontong.utils.network.CheckNetwork
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun apiConfig(): ApiService = Retrofit.Builder()
+        .baseUrl(Constant.BASE_URL)
+        .client(
+            OkHttpClient().newBuilder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
+        )
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .build()
+        .create(ApiService::class.java)
+
+    @Provides
+    fun tanggalDanWaktu(): TanggalDanWaktu = TanggalDanWaktu()
+
+    @Provides
+    fun loading(): LoadingAlertDialog = LoadingAlertDialog()
+
+    @Provides
+    @Singleton
+    fun kataAcak(): KataAcak = KataAcak()
+
+    @Provides
+    @Singleton
+    fun checkNetwork(): CheckNetwork = CheckNetwork()
+
+}
