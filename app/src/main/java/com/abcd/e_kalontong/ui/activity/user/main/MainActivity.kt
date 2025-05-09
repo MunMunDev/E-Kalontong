2a1dff31269d270cc3fca7ddd7094dfd9dd67fa9
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-09 13:00:20 +0800

    Initial first commit

---
+++ app/src/main/java/com/abcd/e_kalontong/ui/activity/user/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abcd.e_kalontong.ui.activity.user.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abcd.e_kalontong.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/abcd/e_kalontong/ui/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.abcd.e_kalontong.ui.splash_screen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.abcd.e_kalontong.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
