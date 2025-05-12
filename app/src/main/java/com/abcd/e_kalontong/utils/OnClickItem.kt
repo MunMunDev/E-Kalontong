18bdc114ff0a3991be9ae66482dd39b06d2aff83
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-12 14:03:24 +0800

    commit add home fragment.

--- app/src/main/java/com/abcd/e_kalontong/utils/OnClickItem.kt
+++ app/src/main/java/com/abcd/e_kalontong/utils/OnClickItem.kt
@@ -2,10 +2,16 @@ package com.abcd.e_kalontong.utils
 
 import android.view.View
 import com.abcd.e_kalontong.data.model.PesananModel
+import com.abcd.e_kalontong.data.model.ProdukModel
 
 interface OnClickItem {
     interface ClickPesanan{
         fun clickItemPesanan(pesanan: PesananModel, it: View)
         fun clickGambarPesanan(gambar: String, produk:String, it: View)
+    }
+
+    interface ClickProduk{
+        fun clickItemPesan(produk: ProdukModel, it: View)
+        fun clickGambarProduk(gambar: String, produk:String, it: View)
     }
 }
