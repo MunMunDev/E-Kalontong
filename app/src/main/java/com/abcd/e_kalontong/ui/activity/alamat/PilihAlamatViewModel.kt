cc6dd9ad036235798cd8ed8f04e2306d9cdbd1a0
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-13 22:30:05 +0800

    commit add pilih alamat and add additions to the activity. list, adapter, etc

---
+++ app/src/main/java/com/abcd/e_kalontong/ui/activity/alamat/PilihAlamatViewModel.kt
@@ -0,0 +1,112 @@
+package com.abcd.e_kalontong.ui.activity.alamat
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abcd.e_kalontong.data.database.api.ApiService
+import com.abcd.e_kalontong.data.model.AlamatModel
+import com.abcd.e_kalontong.data.model.KecamatanModel
+import com.abcd.e_kalontong.data.model.ResponseModel
+import com.abcd.e_kalontong.utils.network.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PilihAlamatViewModel @Inject constructor(
+    private val api: ApiService
+): ViewModel() {
+    val _alamatUser = MutableLiveData<UIState<ArrayList<AlamatModel>>>()
+    val _kecamatan = MutableLiveData<UIState<ArrayList<KecamatanModel>>>()
+    val _updateMainAlamat = MutableLiveData<UIState<ArrayList<ResponseModel>>>()
+    val _tambahAlamatUser = MutableLiveData<UIState<ArrayList<ResponseModel>>>()
+    val _updateAlamatUser = MutableLiveData<UIState<ArrayList<ResponseModel>>>()
+
+    fun fetchDataAlamat(idUser:Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            _alamatUser.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val dataAlamat = api.getAlamatUser("", idUser)
+                _alamatUser.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _alamatUser.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
+    fun fetchKecamatan(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _kecamatan.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val dataAlamat = api.getKecamatan("")
+                _kecamatan.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _kecamatan.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
+    fun postUpdateMainAlamat(
+        idAlamat: String, idUser: String
+    ){
+        viewModelScope.launch(Dispatchers.IO) {
+            _updateMainAlamat.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val dataAlamat = api.postUpdateMainAlamat(
+                    "", idAlamat, idUser
+                )
+                _updateMainAlamat.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _updateMainAlamat.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
+    fun postTambahAlamat(
+        idUser: String, namaLengkap: String, nomorHp: String,
+        idKecamatan: String, detailAlamat: String
+    ){
+        viewModelScope.launch(Dispatchers.IO) {
+            _tambahAlamatUser.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val dataAlamat = api.postTambahAlamat(
+                    "", idUser, namaLengkap, nomorHp, idKecamatan, detailAlamat
+                )
+                _tambahAlamatUser.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _tambahAlamatUser.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
+    fun postUpdateAlamat(
+        idAlamat: String, idUser: String, namaLengkap: String,
+        nomorHp: String, idKecamatan: String, detailAlamat: String
+    ){
+        viewModelScope.launch(Dispatchers.IO) {
+            _updateAlamatUser.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val dataAlamat = api.postUpdateAlamat(
+                    "", idAlamat, idUser, namaLengkap, nomorHp, idKecamatan, detailAlamat
+                )
+                _updateAlamatUser.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _updateAlamatUser.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
+    fun getDataAlamat(): LiveData<UIState<ArrayList<AlamatModel>>> = _alamatUser
+    fun getKecamatan(): LiveData<UIState<ArrayList<KecamatanModel>>> = _kecamatan
+    fun getUpdateMainAlamat(): LiveData<UIState<ArrayList<ResponseModel>>> = _updateMainAlamat
+    fun getTambahAlamat(): LiveData<UIState<ArrayList<ResponseModel>>> = _tambahAlamatUser
+    fun getUpdateAlamat(): LiveData<UIState<ArrayList<ResponseModel>>> = _updateAlamatUser
+}
