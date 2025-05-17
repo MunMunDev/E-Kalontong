4b5037bae9a146fd79991057da4a0c89035607da
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-17 11:52:41 +0800

    commit update login activity. Add button to register activity

--- app/src/main/java/com/abcd/e_kalontong/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/abcd/e_kalontong/ui/activity/login/LoginActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.abcd.e_kalontong.R
 import com.abcd.e_kalontong.data.model.UserModel
 import com.abcd.e_kalontong.databinding.ActivityLoginBinding
+import com.abcd.e_kalontong.ui.activity.register.RegisterActivity
 import com.abcd.e_kalontong.ui.activity.user.main.MainActivity
 import com.abcd.e_kalontong.utils.LoadingAlertDialog
 import com.abcd.e_kalontong.utils.SharedPreferencesLogin
@@ -41,24 +38,34 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun button(){
-//        btnDaftar()
-        btnLogin()
+        loginBinding.apply {
+            btnLogin.setOnClickListener {
+                btnLogin()
+            }
+            tvDaftar.setOnClickListener {
+                btnDaftar()
+            }
+        }
+    }
+
+    private fun btnDaftar() {
+        loginBinding.tvDaftar.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
     }
 
     private fun btnLogin() {
         loginBinding.apply {
-            btnLogin.setOnClickListener{
-                if(etUsername.text.isNotEmpty() && etPassword.text.isNotEmpty()){
-                    loading.alertDialogLoading(this@LoginActivity)
-                    fetchUsers(etUsername.text.toString(), etPassword.text.toString())
+            if(etUsername.text.isNotEmpty() && etPassword.text.isNotEmpty()){
+                loading.alertDialogLoading(this@LoginActivity)
+                fetchUsers(etUsername.text.toString(), etPassword.text.toString())
+            }
+            else{
+                if(etUsername.text.isEmpty()){
+                    etUsername.error = "Masukkan Username"
                 }
-                else{
-                    if(etUsername.text.isEmpty()){
-                        etUsername.error = "Masukkan Username"
-                    }
-                    if(etPassword.text.isEmpty()){
-                        etPassword.error = "Masukkan Password"
-                    }
+                if(etPassword.text.isEmpty()){
+                    etPassword.error = "Masukkan Password"
                 }
             }
         }
@@ -122,6 +129,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private var tapDuaKali = false
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (tapDuaKali){
             super.onBackPressed()
