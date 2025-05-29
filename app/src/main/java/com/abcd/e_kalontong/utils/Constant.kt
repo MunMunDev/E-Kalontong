91d1546d88560460f290aacea65c49112c79e9d4
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-05-29 15:29:42 +0800

    Update all data

--- app/src/main/java/com/abcd/e_kalontong/utils/Constant.kt
+++ app/src/main/java/com/abcd/e_kalontong/utils/Constant.kt
@@ -1,13 +1,14 @@
 package com.abcd.e_kalontong.utils
 
 object Constant {
-    const val BASE_URL = "http://192.168.17.4/"
+//    const val BASE_URL = "https://192.168.17.4/"
+    const val BASE_URL = "https://e-portofolio.web.id/"
     const val LOCATION_GAMBAR = "$BASE_URL/e-kelontong/gambar/"
 
     const val STORAGE_PERMISSION_CODE = 10
     const val CAMERA_PERMISSION_CODE = 11
 
     const val MIDTRANS_BASE_URL = "https://e-portofolio.web.id/e-kelontong/api/response-midtrans.php/"
-    const val MIDTRANS_SERVER_KEY = "\"SB-Mid-server-wA8kiwX70GwIoIOmlWkG5H-6\""
-    const val MIDTRANS_CLIENT_KEY = "SB-Mid-client-XKAPR8Xd9KBrS2m3"
+    const val MIDTRANS_SERVER_KEY = "SB-Mid-server-sWCHmQJL9eOk2uaZxMzfvYL8"
+    const val MIDTRANS_CLIENT_KEY = "SB-Mid-client-i_YpsHvMh0mEpxFQ"
 }
