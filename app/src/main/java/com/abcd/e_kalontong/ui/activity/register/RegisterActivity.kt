15a2e0f18b0c9880728821d7dac09f7b915fffe2
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-06-17 15:10:29 +0800

    do an update. add the address and sub-district in the user register and the data will also be sent to the model address (alamat model).

--- app/src/main/java/com/abcd/e_kalontong/ui/activity/register/RegisterActivity.kt
+++ app/src/main/java/com/abcd/e_kalontong/ui/activity/register/RegisterActivity.kt
@@ -1,20 +1,20 @@
 package com.abcd.e_kalontong.ui.activity.register
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.abcd.e_kalontong.R
+import com.abcd.e_kalontong.data.model.KecamatanModel
 import com.abcd.e_kalontong.data.model.ResponseModel
 import com.abcd.e_kalontong.databinding.ActivityRegisterBinding
-import com.abcd.e_kalontong.ui.activity.login.LoginActivity
 import com.abcd.e_kalontong.utils.LoadingAlertDialog
 import com.abcd.e_kalontong.utils.network.UIState
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.ArrayList
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -23,13 +23,78 @@ class RegisterActivity : AppCompatActivity() {
     @Inject
     lateinit var loading: LoadingAlertDialog
     private val viewModel : RegisterViewModel by viewModels()
+    private lateinit var listKecamatan: ArrayList<KecamatanModel>
+    private var selectedKecamatan = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fetchKecamatan()
         setButton()
         postRegisterUser()
+        getKecamatan()
+    }
+
+    private fun fetchKecamatan() {
+        viewModel.fetchKecamatan()
+    }
+
+    private fun getKecamatan(){
+        viewModel.getKecamatan().observe(this@RegisterActivity){result->
+            when(result){
+                is UIState.Loading->{}
+                is UIState.Success-> setSuccessFetchKecamatan(result.data)
+                is UIState.Failure-> setFailureFetchKecamatan(result.message)
+            }
+        }
+    }
+
+    private fun setFailureFetchKecamatan(message: String) {
+        Log.e("RegisterActivityTAG", "setFailureFetchKecamatan: $message ")
+    }
+
+    private fun setSuccessFetchKecamatan(data: ArrayList<KecamatanModel>) {
+        if(data.isNotEmpty()){
+            listKecamatan = data
+            val listNamaKecamatan: ArrayList<String> = arrayListOf()
+            for(value in data){
+                listNamaKecamatan.add(value.kecamatan!!)
+                Log.d("RegisterActivityTAG", "data: $value: ")
+            }
+            if(listNamaKecamatan.isNotEmpty()){
+                Log.d("RegisterActivityTAG", "ada isi nya: ")
+                setSpinnerKecamatan(listNamaKecamatan)
+            }
+        }
+    }
+
+    private fun setSpinnerKecamatan(listNamaKecamatan: ArrayList<String>) {
+        binding.apply {
+            val arrayAdapterKecamatan = ArrayAdapter(
+                this@RegisterActivity,
+                android.R.layout.simple_spinner_item,
+                listNamaKecamatan
+            )
+
+            arrayAdapterKecamatan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spKecamatan.adapter = arrayAdapterKecamatan
+
+            spKecamatan.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    selectedKecamatan = listKecamatan[position].id_kecamatan!!
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+        }
     }
 
     private fun setButton() {
@@ -54,6 +119,10 @@ class RegisterActivity : AppCompatActivity() {
                 etEditNomorHp.error = "Tidak Boleh Kosong"
                 cek = true
             }
+            if (etDetailAlamat.toString().isEmpty()) {
+                etDetailAlamat.error = "Tidak Boleh Kosong"
+                cek = true
+            }
             if (etEditUsername.toString().isEmpty()) {
                 etEditUsername.error = "Tidak Boleh Kosong"
                 cek = true
@@ -67,15 +136,22 @@ class RegisterActivity : AppCompatActivity() {
                 postTambahData(
                     etEditNama.text.toString().trim(),
                     etEditNomorHp.text.toString().trim(),
+                    selectedKecamatan,
+                    etDetailAlamat.text.toString().trim(),
                     etEditUsername.text.toString().trim(),
-                    etEditPassword.text.toString().trim()
+                    etEditPassword.text.toString().trim(),
                 )
             }
         }
     }
 
-    private fun postTambahData(nama: String, nomorHp: String, username: String, password: String){
-        viewModel.postRegisterUser(nama, nomorHp, username, password, "user")
+    private fun postTambahData(
+        nama: String, nomorHp: String, idKecamatan: String, detailAlamat: String,
+        username: String, password: String
+    ){
+        viewModel.postRegisterUser(
+            nama, nomorHp, idKecamatan, detailAlamat, username, password, "user"
+        )
     }
 
     private fun postRegisterUser(){
--- app/src/main/java/com/abcd/e_kalontong/ui/activity/register/RegisterViewModel.kt
+++ app/src/main/java/com/abcd/e_kalontong/ui/activity/register/RegisterViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.abcd.e_kalontong.data.database.api.ApiService
+import com.abcd.e_kalontong.data.model.KecamatanModel
 import com.abcd.e_kalontong.data.model.ResponseModel
 import com.abcd.e_kalontong.utils.network.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,11 +19,17 @@ class RegisterViewModel @Inject constructor(
     private val api: ApiService
 ): ViewModel() {
     private var _registerUser = MutableLiveData<UIState<ResponseModel>>()
+    private var _kecamatan = MutableLiveData<UIState<ArrayList<KecamatanModel>>>()
 
-    fun postRegisterUser(nama:String, nomorHp:String, username:String, password:String, sebagai:String){
+    fun postRegisterUser(
+        nama:String, nomorHp:String, idKecamatan:String, detailAlamat:String,
+        username:String, password:String, sebagai:String
+    ){
         viewModelScope.launch(Dispatchers.IO) {
+            _registerUser.postValue(UIState.Loading)
+            delay(1_000)
             try {
-                val registerUser = api.addUser("", nama, nomorHp, username, password, sebagai)
+                val registerUser = api.addUser("", nama, nomorHp, idKecamatan, detailAlamat, username, password, sebagai)
                 _registerUser.postValue(UIState.Success(registerUser))
             } catch (ex: Exception){
                 _registerUser.postValue(UIState.Failure("Gagal : ${ex.message}"))
@@ -29,5 +37,18 @@ class RegisterViewModel @Inject constructor(
         }
     }
 
+    fun fetchKecamatan(){
+        viewModelScope.launch(Dispatchers.IO) {
+//            _kecamatan.postValue(UIState.Loading)
+            try {
+                val dataAlamat = api.getKecamatan("")
+                _kecamatan.postValue(UIState.Success(dataAlamat))
+            } catch (ex: Exception){
+                _kecamatan.postValue(UIState.Failure("Error pada: ${ex.message}"))
+            }
+        }
+    }
+
     fun getRegisterUser(): LiveData<UIState<ResponseModel>> = _registerUser
+    fun getKecamatan(): LiveData<UIState<ArrayList<KecamatanModel>>> = _kecamatan
 }
